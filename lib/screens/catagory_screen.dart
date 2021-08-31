e48800f6eb3db324198de60ffe7ff776d0a32e4b
sajal48 <newtonh48@gmail.com>
2021-08-31 15:33:26 +0600

    small changes

--- lib/screens/catagory_screen.dart
+++ lib/screens/catagory_screen.dart
@@ -26,71 +26,69 @@ class _CatagoryScreenState extends State<CatagoryScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.fromLTRB(34, 0, 34, 50),
         height: double.infinity,
         width: double.infinity,
         color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(34, 0, 34, 50),
-          child: Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Choose your\nCatagory',
-                  style: TextStyle(
-                      fontFamily: 'Milliard',
-                      fontSize: 54,
-                      color: Color(0xff3D4C59),
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => BoardAndClassScreen()));
-                      },
-                      child: Category(
-                        index: 0,
-                        isSelected: true,
-                      ),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Choose your\nCatagory',
+                style: TextStyle(
+                    fontFamily: 'Milliard',
+                    fontSize: 54,
+                    color: Color(0xff3D4C59),
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BoardAndClassScreen()));
+                    },
+                    child: Category(
+                      index: 0,
+                      isSelected: true,
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => (BoardSelectScreen())));
-                      },
-                      child: Category(
-                        index: 1,
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Category(
-                      index: 2,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => (BoardSelectScreen())));
+                    },
+                    child: Category(
+                      index: 1,
                     ),
-                    Category(
-                      index: 3,
-                    )
-                  ],
-                )
-              ],
-            ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Category(
+                    index: 2,
+                  ),
+                  Category(
+                    index: 3,
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
